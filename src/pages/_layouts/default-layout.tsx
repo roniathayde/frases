31f8a7bd2c8ef20ed3bcb477d6bd10fe35a71c14
roniathayde <pontes014@gmail.com>
2024-02-20 21:03:36 -0300

    add a menu hambuguer with radix element

--- src/pages/_layouts/default-layout.tsx
+++ src/pages/_layouts/default-layout.tsx
@@ -1,4 +1,5 @@
-import { Fingerprint, Text } from 'lucide-react'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+import { Fingerprint, Menu, Text } from 'lucide-react'
 import { Link, Outlet } from 'react-router-dom'
 
 export function DefaultLayout() {
@@ -8,16 +9,53 @@ export function DefaultLayout() {
         <h1 className="text-lg font-medium">Guilherme Fernandes</h1>
 
         <div className="flex gap-6">
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger asChild>
+              <button
+                className="flex items-center gap-2 rounded bg-slate-900/60 px-2 py-1.5 transition-colors hover:bg-slate-700 lg:hidden"
+                aria-label="Customise options"
+              >
+                <Menu className="size-5" />
+              </button>
+            </DropdownMenu.Trigger>
+            <DropdownMenu.Portal>
+              <DropdownMenu.Content
+                className="rounded bg-slate-800 p-2"
+                sideOffset={15}
+              >
+                <DropdownMenu.Item className="">
+                  <Link
+                    to="/"
+                    className="flex items-center gap-2 rounded bg-slate-900/60 px-2 py-1.5 text-zinc-200 transition-colors hover:bg-slate-700 lg:hidden"
+                  >
+                    <Text className="size-4" />
+                    Frases
+                  </Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator className=" rounded-full  border-b-2 border-slate-600" />
+
+                <DropdownMenu.Item className="">
+                  <Link
+                    to="/biografia"
+                    className=" flex items-center gap-2 rounded bg-slate-900/60 px-2 py-1.5 text-zinc-200 transition-colors hover:bg-slate-700 lg:hidden"
+                  >
+                    <Fingerprint className="size-4" />
+                    Biografia
+                  </Link>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Portal>
+          </DropdownMenu.Root>
           <Link
             to="/"
-            className="flex items-center gap-2 rounded bg-slate-900/60 px-2 py-1.5 transition-colors hover:bg-slate-700"
+            className="hidden items-center gap-2 rounded bg-slate-900/60 px-2 py-1.5 transition-colors hover:bg-slate-700 lg:flex"
           >
             <Text className="size-4" />
             Frases
           </Link>
           <Link
             to="/biografia"
-            className="flex items-center gap-2 rounded bg-slate-900/60 px-2 py-1.5 transition-colors hover:bg-slate-700"
+            className="hidden items-center gap-2 rounded bg-slate-900/60 px-2 py-1.5 transition-colors hover:bg-slate-700 lg:flex"
           >
             <Fingerprint className="size-4" />
             Biografia
